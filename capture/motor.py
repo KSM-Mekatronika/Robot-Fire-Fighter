1a68f691aaad346c45f6352332d171c1ca220f40
BiggusMaximus <annastyabagas164@gmail.com>
2022-10-13 15:02:04 +0700

    adding pwm

---
+++ capture/motor.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+
+IN1 = 14
+IN2 = 15
+IN3 = 20
+IN4 = 21
+ENA = 18
+ENB = 12
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+
+GPIO.output(IN1, GPIO.LOW)
+GPIO.output(IN2, GPIO.LOW)
+GPIO.output(IN3, GPIO.LOW)
+GPIO.output(IN4, GPIO.LOW)
+
+PWM1 = GPIO.PWM(ENA, 1000)
+PWM2 = GPIO.PWM(ENB, 1000)
+PWM1.start(0)
+PWM2.start(0)
+
+while True:
+    PWM1.ChangeDutyCycle(75)
+    PWM2.ChangeDutyCycle(75)
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
